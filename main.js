ce2a6030fadbc760b870410a77753f52cbe71367
fxzqf <45503962+fxzqf@users.noreply.github.com>
2022-07-26 21:40:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//这个文件由index.html包含
+
+/*document.write("<script language='javascript' src='js/util.js'></script>");
+document.write("<script language='javascript' src='js/ribbon.js'></script>");
+document.write("<script language='javascript' src='js/systemdemo.js'></script>");
+*/
 function newWorkBook()
 {
     App=wps.EtApplication().Application;
