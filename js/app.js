ac291283a8f6c86d0abe6f9ea03f8057e9f78e3e
fxzqf <fxzqf@163.com>
2022-07-27 20:52:34 +0800

    1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 "use strict";
 /// <reference path="./src/index.d.ts" />
-window.onload = () => {
-    let url = location.href.split("?")[1];
-    let parames = new URLSearchParams(url);
-    //wps.EtApplication().Application
-};
+function openBook(obj) {
+    //let App=wps.EtApplication().Application;
+    //App.Workbooks.Add();
+    wps.CreateTaskPane("https://zhibiao.uicp.fun/", "表格助手").Visible = true;
+    //aap.Visible=true;
+}
+/**
+* 获取一个控件的图标
+* @param control 要获取图标的控件
+* @returns 图标的SVG图像的URL
+*/
+function GetImage(control) {
+    var eleId = control.Id;
+    switch (eleId) {
+        case "btnShowMsg":
+            return "../images/1.svg";
+        case "btnShowDialog":
+            return "../images/2.svg";
+        case "btnShowTaskPane":
+            return "../images/3.svg";
+        default:
+            ;
+    }
+    return "../images/newFromTemp.svg";
+}
--- ts/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/// <reference path="./src/index.d.ts" />
-window.onload = () => {
-    let url = location.href.split("?")[1];
-    let parames = new URLSearchParams(url);
-    //wps.EtApplication().Application
-};
