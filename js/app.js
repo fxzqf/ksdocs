8477b46cc4c660dbe815df897eb7781bf5e3a3d9
fxzqf <fxzqf@163.com>
2022-07-28 13:55:17 +0800

    1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference path="./src/index.d.ts" />
 function OnAddinLoad(ribbonUI) {
-    alert("Good");
+    return true;
 }
 function openBook(obj) {
     //wps.PluginStorage.getItem()
